7d146dd538a1b532b1cbe763fb056f66523422b1
Mr Robot <eliasbzr@gmail.com>
2024-08-05 18:59:12 -0300

    iniciando com docstrings de uma linha

---
+++ Aula156/main.py
@@ -0,0 +1,31 @@
+import uma_linha
+
+# docstrings são variaveis que tem 3 aspas ''' """ sejam simples ou duplas
+
+
+
+# print(dir(uma_linha)) #com esse cod me mostra tudo o q tenho dentro do modulo
+
+
+#dessa forma fica mais organizado como uma lista
+# for d in dir(uma_linha):
+#     print (d)
+
+# print(uma_linha.__doc__)
+# print(uma_linha.__file__)
+# print(uma_linha.__package__)
+
+
+help(uma_linha) #com a funcao help consigo ver tudo o que esta dentro do modulo
+
+# NAME
+#     uma_linha
+
+# FUNCTIONS
+#     Falar()
+
+# DATA
+#     variavel = 'valor'
+
+# FILE
+#     c:\users\elias\documents\cursos\udemy\python do basico ao avancado\projeto\aula156\uma_linha.py
---
+++ Aula156/uma_linha.py
@@ -0,0 +1,9 @@
+#  docstrings sao variaveis com 3 aspas, sejam simples ou duplas
+
+''' Documentacao do modulo - explica o que o modulo faz'''
+
+def Falar ():
+    print('Olá')
+
+
+variavel = 'valor'
